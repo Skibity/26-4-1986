392acb8d558792f668e257878b24f38335a4dc6d
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-08-30 20:04:19 -0300

    new

---
+++ script.js
@@ -0,0 +1,14 @@
+function verificarSenha(){
+
+  var senha = document.getElementById('senha').value;
+  var container = document.querySelector('.container');
+  var video = document.querySelector('.video');
+
+
+  if (senha === "123"){
+    container.style.display="none";
+    video.style.display="flex";
+  } else {
+    alert("Senha " + senha + " está errada, tente novamente")
+  }
+}
