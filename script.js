682a2cd05d33ac694a1e61fed6431058b7b3a5b8
hail.azazel <102439387+Skibity@users.noreply.github.com>
2023-10-05 08:49:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,76 @@
 const input = document.querySelector(".input");
 
 input.focus();
-
-window.addEventListener("click", () => input.focus());
+window.addEventListener("click", () => input.focus())
 
 function getInputValue() {
-
   return document.querySelector(".input").innerHTML;
-
 }
 
 function write(el, text, cb) {
-
   let index = 0;
-
   el.innerHTML = "";
 
-  function append() {
-
+  return function append() {
     if (index >= text.length) {
-
       cb();
-
-      return;
-
+      return
     }
-
     setTimeout(() => {
-
-      el.innerHTML = el.innerHTML.concat(text[index]);
-
+      el.innerHTML = el.innerHTML.concat(text[index])
       index++;
-
-      append();
-
-    }, Math.floor(Math.random() * (300 - 100 + 1) + 100));
-
+      append()
+    }, Math.floor(Math.random() * (300 - 100 + 1) + 100))
   }
-
-  append();
-
 }
 
-document.addEventListener('keydown', ({ code }) => {
-
+document.addEventListener('keydown', async ({ code }) => {
   if (code === "Enter") {
-
     const inputValue = getInputValue().trim().toLowerCase();
 
     if (inputValue === "--help" || inputValue === "-h") {
-
       document.querySelector(".input").innerHTML = "";
-
       document.querySelector(".input").innerHTML = `
-
         <table>
-
           <tr>
-
-            <th>2222222</th>
-
-            <th>2222222</th>
-
-            <th>2222222</th>
-
+            <th>Command</th>
+            <th>Options</th>
+            <th>Details</th>
           </tr>
-
           <tr>
-
             <td>--h</td>
-
             <td>-</td>
-
             <td>Display all commands</td>
-
           </tr>
-
         </table>
-
       `;
-
       return;
-
     } else if (inputValue === "login") {
+      // Show "Processing, please wait..." while loading
+      document.querySelector(".input").innerHTML = "";
+      const processInput = write(document.querySelector(".prefix"), "Processing, please wait...", async () => {
+        // Load the login page
+        const response = await fetch("/login.html");
+        const loginPageContent = await response.text();
+        
+        // Replace the current page content with the login page content
+        document.querySelector(".prefix").innerHTML = "";
+        document.querySelector(".input").innerHTML = loginPageContent;
+      });
+      
+      processInput();
+    } else {
+      document.querySelector(".input").innerHTML = "";
+      document.querySelector(".input").setAttribute("contenteditable", false);
 
-      window.location.href = "/login.html"; // Redirect to the login subpage
-
-      return;
-
-    }
-
-    document.querySelector(".input").innerHTML = "";
-
-    document.querySelector(".input").setAttribute("contenteditable", false);
-
-    const processInput = write(document.querySelector(".prefix"), "Processing, please wait...", () => {
-
-      const typeResponse = write(document.querySelector(".prefix"), "C:/User/admin >", () => {
-
-        document.querySelector(".input").setAttribute("contenteditable", true);
-
+      const processInput = write(document.querySelector(".prefix"), "Processing, please wait...", () => {
+        const typeResponse = write(document.querySelector(".prefix"), "C:/User/admin >", () => {
+          document.querySelector(".input").setAttribute("contenteditable", true);
+        });
+        typeResponse();
       });
 
-      typeResponse();
-
-    });
-
-    processInput();
-
+      processInput();
+    }
   }
-
 });
-
